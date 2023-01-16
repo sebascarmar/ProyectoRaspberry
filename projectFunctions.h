4f28d39b2213f4ea39f22b6ba384dbae49435158
sebacarmar <sebacarmar@gmail.com>
2023-01-16 13:30:17 -0300

    Agrega archivo de cabecera de las funcions propias

---
+++ projectFunctions.h
@@ -0,0 +1,20 @@
+#ifndef PROJECTFUNCTIONS_H
+#define PROJECTFUNCTIONS_H
+
+
+#define FD_STDIN 0
+#define LENGTH_PSSW 5
+
+/* FUNCION:  controlDeContraseña
+ * ACCION: realiza un control de acceso al programa.
+ * PARAMETROS: ninguno.
+ * RETORNO: retorna un entero, 1 si se logra acceder y 0 si no.
+ */
+int controlDeContraseña( void );
+
+
+
+
+
+
+#endif

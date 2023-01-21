28fa4e55ec5a7a734094e5c3482c9469e5ed9e02
sebascarmar <sebacarmar@gmail.com>
2023-01-20 23:03:14 -0300

    Elimina defniciones de funciones y variables

--- easyPio.h
+++ easyPio.h
@@ -31,8 +31,6 @@
 #define GPIO_BASE           (BCM2835_PERI_BASE + 0x200000) // Dir. base del GPIO.
 #define BLOCK_SIZE          (4*1024)
 
-// Punteros  que serán mapeados en memeoria cuando se llama a pioInit().
-volatile unsigned int       *gpio; // Puntero a la base de GPIO.
 
 /*###############################################################################*/
 /*##                             Registros GPIO                                ##*/
@@ -45,70 +43,17 @@ volatile unsigned int       *gpio; // Puntero a la base de GPIO.
 /*###############################################################################*/
 /*##                          Funciones generales                              ##*/
 /*###############################################################################*/
-void pioInit( )
-{
-  int mem_fd;
-  void *reg_map;
-
-  mem_fd = open("/dev/mem", O_RDWR | O_SYNC); // /dev/mem es un pseudo_driver para acceso 
-                                              //a memoria en Linux.
-  if( mem_fd < 0)                             // Verifica si se pudo abrir el archivo.
-  {
-    printf("Error al abrir /dev/mem.\n");
-    exit(EXIT_FAILURE);
-  }
-
-  reg_map = mmap(
-                 NULL,                        // Dir. desde la cual se inicia el mapeo local.
-                 BLOCK_SIZE,                  // Tamaño del bloque de mem. map. (4kB).
-                 PROT_READ | PROT_WRITE,      // Habilita escr./lect. para la mem. map.
-                 MAP_SHARED,                  // Acceso no exclusivo a esta memoria.
-                 mem_fd,                      // Puntero a /dev/mem.
-                 GPIO_BASE);                  // Offset al periférico GPIO.
-  if (reg_map == MAP_FAILED)                  // Verifica si se pudo realizar el mapeo.
-  {
-    printf("Error al  mapear GPIO  %d\n", (int)reg_map);
-    close(mem_fd);
-    exit(EXIT_FAILURE);
-  }
-
-  gpio = (volatile unsigned *)reg_map; // Asigna mediante un cast el bloque de mem. mapeada 
-                                       //y configurada.
-  close(mem_fd);
-}
+void pioInit( void );
 
 
 /*###############################################################################*/
 /*##                         Funcionetes GPIO                                  ##*/
 /*###############################################################################*/
 
-void pinModeEP( int pin, int function ) // Los registros GPFSEL determinan la fun. de cada pin.
-{
-  int reg    = pin/10;     // 10 GPIO por registro (es decir, elige el registro).
-  int offset = (pin%10)*3; // 3 pines para cada GPIO (selecciona los pines correspondientes).
-  
-  GPFSEL[reg]  &= ~( (0b111 & ~ function) << offset ); // Setea los ceros.
-  GPFSEL[reg]  |= ( (0b111 &  function) << offset );   // Setea los unos.
-}
+void pinModeEP( int pin, int function ); // Los registros GPFSEL determinan la fun. de cada pin.
 /*-------------------------------------------------------------------------------*/ 
-void digitalWrite( int pin, int val )
-{
-  int reg    = pin/32;     // 10 GPIO por registro (es decir, elige el registro).
-  int offset = pin%32; // 3 pines para cada GPIO (selecciona los pines correspondientes).
-
-  if(val)
-    GPSET[reg] = 1 << offset; // Escribe si es HIGH.
-  else
-    GPCLR[reg] = 1 << offset; // Escribe si es LOW.
-}
+void digitalWriteEP( int pin, int val );
 /*-------------------------------------------------------------------------------*/ 
-int digitalRead( int pin )
-{
-  int reg    = pin/32;
-  int offset = pin%32;
-
-  return ( GPLEV[reg] >> offset ) & 0x00000001; // Pone en cero todos los bits que
-						//no interesan.
-}
+int digitalReadEP( int pin );
 
 #endif

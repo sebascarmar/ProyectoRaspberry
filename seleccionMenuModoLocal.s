ab14d9048d2d8414534d6a5b88c1a2a6796ad182
sebascarmar <sebacarmar@gmail.com>
2023-01-23 09:01:11 -0300

    Corrige formato  de código y comentarios

--- seleccionMenuModoLocal.s
+++ seleccionMenuModoLocal.s
@@ -1,6 +1,6 @@
 .data
 enunciado: .ascii "Por favor, ingrese una opción: "
-seleccion: .ascii "\0\0\0\0"
+bufferIngreso: .ascii "\0\0\0\0"
 msjError: .ascii "\nValor inválido. "
 
 .arm
@@ -9,138 +9,144 @@ msjError: .ascii "\nValor inválido. "
 
 
 seleccionMenuModoLocal:
-    PUSH {R4, R7, LR}          // Apila reg. en el stack
+    PUSH {R4, R7, LR} // Apila registros en el stack.
       
-        /**************** IMPRIME EN PANTALLA EL ENUNCIADO ********************/
-        MOV   R7, #4      // 4 es el código de la llamada a 'write' del sistema.
-        MOV   R0, #1      // Descriptor de archivo de stdout (monitor).
-        LDR   R1, =enunciado // Buffer de salida
-        MOV   R2, #32     // Cantidad de char a escribir.
-        SWI   0           // Llamada al sistema para arm 32-bit/EABI.
-        
     loop:
-        /********************** VACÍA EL BUFFER *******************************/
+        /********************** IMPRIME EN PANTALLA EL ENUNCIADO ***********************/
+        MOV   R7, #4          // 4 es el código de la llamada a 'write' del sistema.
+        MOV   R0, #1          // Descriptor de archivo de stdout (monitor).
+        LDR   R1, =enunciado  // Buffer de salida.
+        MOV   R2, #32         // Cantidad de char a escribir.
+        SWI   0               // Llamada al sistema para arm 32-bit/EABI.
+        
+        
+        /****************************** VACÍA EL BUFFER ********************************/
         MOV   R0, #'\0'
-        LDR   R1, =seleccion // Buffer de entrada.
-        STR   R0, [R1]
+        LDR   R1, =bufferIngreso // Buffer de entrada.
+        STR   R0, [R1]           // Escribe '\0' en bufferIngreso[0].
+      
+      
+        /************ ESPERA POR EL INGRESO DE UNA OPCION POR EL TECLADO ***************/
+        bucleRead:
+           MOV   R7, #3             // 3 es el código de la llamada a 'read' del sistema.
+           MOV   R0, #0             // Descriptor de archivo de stdin (teclado).
+           LDR   R1, =bufferIngreso // Buffer de entrada.
+           MOV   R2, #3             // Cantidad de char a leer.
+           SWI   0                  // Llamada al sistema para arm 32-bit/EABI.
+           CMP   R0, #0    
+           BEQ   bucleRead          // Si read devuelve 0 char leídos, sigue en el bucle.
+        
+        LDR   R4, [R1]           // Almacena el char ingresado en R4 (reg. p/ switch).
+      
+      
+        /** IMPRIME EN PANTALLA LO INGRESADO (SOLO CHAR IMPRIMIBLES: de ' ' hasta '~')**/
+         CMP   R4, #'\0'          // Comprueba si el buffer está  vacío, así no imprime.
+         BEQ   opcion1
+         CMP   R4, #32            // Código hasta del caracter espacio.
+         BLT   opcion1
+         CMP   R4, #126           // Código hasta del caracter virgulilla.
+         BGT   opcion1
+         MOV   R7, #4             // 4 es el código de la llamada a 'write' del sistema.
+         MOV   R0, #1             // Descriptor de archivo de stdout (monitor).
+         LDR   R1, =bufferIngreso // Buffer de salida
+         MOV   R2, #1             // Cantidad de char a escribir.
+         SWI   0                  // Llamada al sistema para arm 32-bit/EABI.
 
 
-        /*********** ESPERA POR EL INGRESO DE UNA OPCION **********************/
-    bucleWrite:
-        MOV   R7, #3      // 3 es el código de la llamada a 'read' del sistema.
-        MOV   R0, #0      // Descriptor de archivo de stdin (teclado).
-        LDR   R1, =seleccion // Buffer de entrada.
-        MOV   R2, #3     // Cantidad de char a leer.
-        SWI   0           // Llamada al sistema para arm 32-bit/EABI.
-        LDR   R4, [R1] // Almacena el caracter ingresado en R4.
-        CMP   R0, #0
-        BEQ   bucleWrite
 
-        /**************** IMPRIME EN PANTALLA LO INGRESADO *********************/
-        CMP   R4, #'\0' 
-        BEQ   switchCase
-        CMP   R4, #32
-        BLT   switchCase
-        CMP   R4, #126
-        BGT   switchCase
-        MOV   R7, #4      // 4 es el código de la llamada a 'write' del sistema.
-        MOV   R0, #1      // Descriptor de archivo de stdout (monitor).
-        LDR   R1, =seleccion // Buffer de salida
-        MOV   R2, #1    // Cantidad de char a escribir.
-        SWI   0           // Llamada al sistema para arm 32-bit/EABI.
+        /******************************** SWITCH CASE **********************************/
+        opcion1: // Modo local/remoto.
+            CMP   R4, #'a'
+            CMPNE R4, #'A'
+            BNE opcion2
+            MOV   R0, #'a'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+           
+        opcion2: // Velocidad con pote.
+            CMP   R4, #'b'
+            CMPNE R4, #'B'
+            BNE opcion3
+            MOV   R0, #'b'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion3: // Sec. Auto Fantástico.
+            CMP   R4, #'c'
+            CMPNE R4, #'C'
+            BNE opcion4
+            MOV   R0, #'c'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion4: // Sec. Choque.
+            CMP   R4, #'d'
+            CMPNE R4, #'D'
+            BNE opcion5
+            MOV   R0, #'d'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+         
+        opcion5: // Sec. Apilada.
+            CMP   R4, #'e'
+            CMPNE R4, #'E'
+            BNE opcion6
+            MOV   R0, #'e'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion6: // Sec. Carrera.
+            CMP   R4, #'f'
+            CMPNE R4, #'F'
+            BNE opcion7
+            MOV   R0, #'f'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion7: // Sec. Vúmetro.
+            CMP   R4, #'g'
+            CMPNE R4, #'G'
+            BNE opcion8
+            MOV   R0, #'g'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
 
-
-
-        /************************** SWITCH CASE *******************************/
-    switchCase:
-        //LDRB  R2, [R1]      // Compara el 1er char, que es el que importa.
-        MOV   R2, R4
-        //LDRB  R3, [R1, #1]  // Compara el 2do char, debe ser un enter.
-       
-    //opcion1
-        CMP   R2, #'a'
-        CMPNE R2, #'A'
-        BNE opcion2
-        MOV   R0, #'a'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-       
-    opcion2:
-        CMP   R2, #'b'
-        CMPNE R2, #'B'
-        BNE opcion3
-        MOV   R0, #'b'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion3:
-        CMP   R2, #'c'
-        BNE opcion4
-        MOV   R0, #'c'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion4:
-        CMP   R2, #'d'
-        BNE opcion5
-        MOV   R0, #'d'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-     
-    opcion5:
-        CMP   R2, #'e'
-        BNE opcion6
-        MOV   R0, #'e'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion6:
-        CMP   R2, #'f'
-        BNE opcion7
-        MOV   R0, #'f'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion7:
-        CMP   R2, #'g'
-        BNE opcion8
-        MOV   R0, #'g'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-
-    opcion8:
-        CMP   R2, #'h'
-        BNE opcion9
-        MOV   R0, #'h'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion9:
-        CMP   R2, #'i'
-        BNE opcion10
-        MOV   R0, #'i'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion10:
-        CMP   R2, #'j'
-        BNE opcion11
-        MOV   R0, #'j'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-      
-    opcion11:
-        CMP   R2, #'k'
-        BNE opcion12
-        MOV   R0, #'k'    // Valor de retorno de la función.
-        B   break         // Sale del switch case.
-     
-    opcion12:
-        CMP   R2, #'\0'
-        BNE opcionDefecto
-        B   loop    
-     
-    opcionDefecto:
-        MOV   R7, #4      // 4 es el código de la llamada a 'write' del sistema.
-        MOV   R0, #1      // Descriptor de archivo de stdout (monitor).
-        LDR   R1, =msjError // Buffer de salida
-        MOV   R2, #18     // Cantidad de char a escribir.
-        SWI   0           // Llamada al sistema para arm 32-bit/EABI.
-        B   loop
-     
-    break:
-        POP   {R4, R7, LR}        // Desapila reg. del stack.
-        MOV   PC, LR
-    
-    
-    
+        opcion8: // Sec. Juntos por Paridad.
+            CMP   R4, #'h'
+            CMPNE R4, #'H'
+            BNE opcion9
+            MOV   R0, #'h'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion9: // Sec. Gran Moisés.
+            CMP   R4, #'i'
+            CMPNE R4, #'I'
+            BNE opcion10
+            MOV   R0, #'i'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion10: // Sec. Parpadeo.
+            CMP   R4, #'j'
+            CMPNE R4, #'J'
+            BNE opcion11
+            MOV   R0, #'j'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+          
+        opcion11: // Salir del programa.
+            CMP   R4, #'k'
+            CMPNE R4, #'K'
+            BNE opcion12
+            MOV   R0, #'k'    // Valor de retorno de la función.
+            B   break         // Sale del switch case.
+         
+        opcion12: // Si el buffer está vacío no imprime nada, vuelve al loop.
+            CMP   R4, #'\0'
+            BNE opcionDefecto
+            B   loop    
+         
+        opcionDefecto:
+            MOV   R7, #4        // 4 es el código de la llamada a 'write' del sistema.
+            MOV   R0, #1        // Descriptor de archivo de stdout (monitor).
+            LDR   R1, =msjError // Buffer de salida
+            MOV   R2, #18       // Cantidad de char a escribir.
+            SWI   0             // Llamada al sistema para arm 32-bit/EABI.
+            B   loop
+        
+        
+        /**************************** SALIDA DE LA FUNCIÓN *****************************/
+        break:
+            POP   {R4, R7, LR}  // Desapila registros del stack.
+            MOV   PC, LR

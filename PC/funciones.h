3f2f50454bb88dd093ab92478cf24646332c9666
sebacarmar <sebacarmar@gmail.com>
2023-01-25 21:48:39 -0300

    Realiza los comentarios de cada función

--- PC/funciones.h
+++ PC/funciones.h
@@ -9,7 +9,7 @@
 
 #include <stdbool.h>
 
-/*----------------- Descriptor de entrada estándar y salida estándar ----------------------*/
+/*------------------ Descriptor de archivo de los archivos estándar -----------------------*/
 #define FD_STDIN 0
 #define FD_STDOUT 1
 #define FD_STDERR 2
@@ -19,16 +19,18 @@
 /*##                                     PARÁMETROS TTY                                  ##*/
 /*#########################################################################################*/
 
-/* ACCIÓN: setea el modo NO canónico en la entrada estándar.
- * PARÁMETROS: un puntero a la estructura termios de la terminal a configurar.
+/* ACCIÓN: setea el modo NO canónico en la tty apuntada por ttyNew.
+ * PARÁMETROS: un entero con el descriptor de archivos de la tty en cuestión
+ *             un puntero a la estructura termios de la terminal a configurar
  * RETORNO: ninguno.
  */
 void seteoModoNoCanonicoNoBloqueante( int fd, struct termios *ttyNew );
 
 /*******************************************************************************************/
 
-/* ACCIÓN: setea el modo NO canónico en la entrada estándar.
- * PARÁMETROS: un puntero a la estructura termios de la terminal a configurar.
+/* ACCIÓN: setea la trama 8N1 y los baudios de la tty apuntada por ttyNew.
+ * PARÁMETROS: un entero con el descriptor de archivos de la tty en cuestión
+ *             un puntero a la estructura termios de la terminal a configurar
  * RETORNO: ninguno.
  */
 void seteoTramaYBaudRate( int fd, struct termios *ttyNew, speed_t baudRate );
@@ -38,7 +40,7 @@ void seteoTramaYBaudRate( int fd, struct termios *ttyNew, speed_t baudRate );
 /*##                           FUNCIONES PARA EL MENÚ                                    ##*/
 /*#########################################################################################*/
 
-/* ACCIÓN: imprime las opciones del menú principal en la stdout.
+/* ACCIÓN: imprime las opciones del menú principal en la salida estándar.
  * PARÁMETROS: ninguno.
  * RETORNO: ninguno.
  */
@@ -46,16 +48,18 @@ void imprimeMenu( void );
 
 /*******************************************************************************************/
 
-/* ACCIÓN: imprime las opciones del menú principal en la stdout.
- * PARÁMETROS: ninguno.
+/* ACCIÓN: selecciona las opciones ingresadas por el teclado, las envía por puerto
+ *        serie y las imprime en pantalla, haciendo un control de opciones válidas.
+ * PARÁMETROS: un entero con el descriptor de archivos del puerto serie.
  * RETORNO: ninguno.
  */
 char seleccionMenuModoRemoto( int fdUART );
 
 /*******************************************************************************************/
 
-/* ACCIÓN: imprime las opciones del menú principal en la stdout.
- * PARÁMETROS: ninguno.
+/* ACCIÓN: da aviso que se eligió el modo local, y solo permite avanzar si se elige el
+ *        modo remoto nuevamente.
+ * PARÁMETROS: un entero con el descriptor de archivos del puerto serie.
  * RETORNO: ninguno.
  */
 char seleccionMenuModoLocal( void );
@@ -65,8 +69,9 @@ char seleccionMenuModoLocal( void );
 /*##                           ELECCIÓN DE MODO LOCAL O REMOTO                           ##*/
 /*#########################################################################################*/
 
-/* ACCIÓN: seleccion el modo local o remoto, estando en modo local.
- * PARÁMETROS: ninguno.
+/* ACCIÓN: seleccion el modo local o remoto.
+ * PARÁMETROS: un entero con el descriptor de archivos del puerto serie
+ *             un bool indicando si se está en modo local o remoto al momento del llamado
  * RETORNO: un bool: verdadero si es modo local, falso si es modo remoto.
  */
 bool seleccionModo( int fdUART, bool modoLocal );
@@ -76,9 +81,9 @@ bool seleccionModo( int fdUART, bool modoLocal );
 /*##                                SECUENCIAS                                           ##*/
 /*#########################################################################################*/
 
-/* ACCIÓN: seleccion el modo local o remoto, estando en modo local.
- * PARÁMETROS: ninguno.
- * RETORNO: un bool: verdadero si es modo local, falso si es modo remoto.
+/* ACCIÓN: detecta lo ingresado por teclado y lo envía por puerto serie, durante la secuencia
+ * PARÁMETROS: un entero con el descriptor de archivos del puerto serie.
+ * RETORNO: ninguno.
  */
 void secuencia( int fdUART );
 
